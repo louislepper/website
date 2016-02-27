afe042268c6e46a4d9d40f03c3d6c4f3f218ab4b
Louis Lepper <me@louislepper.co.nz>
2016-02-27 22:33:48 +1100

    Styling close to done.

--- main.js
+++ main.js
@@ -13,25 +13,43 @@ var Tagline = React.createClass({
 
 var HeaderSection = React.createClass({
   render: function() {
-    return <div className='HeaderSection'><Name/><Tagline/></div>;
+    return <div className='HeaderSection col-sm-8 col-sm-offset-2'><Name/><Tagline/></div>;
   }
 });
 
+var Row = React.createClass({
+  render: function() {
+    return <div className='row'>{ this.props.children }</div>;
+  }
+})
+
 var ExternalLinksSection = React.createClass({
   render: function() {
-    return <div>Linkedin Youtube Twitter Facebook Bitbucket Github</div>;
+
+    return <div className='ExternalLinksSection col-sm-8 col-sm-offset-2'>
+    <a href="https://www.facebook.com/louislepper" className="social-media-icon fa fa-facebook-square fa-5x"></a>
+    <a href="https://www.linkedin.com/profile/view?id=114463031" className="social-media-icon fa fa-linkedin-square fa-5x"></a>
+    <a href="https://www.youtube.com/channel/UCJd32Ob18St-iB1L_Z7bh1g" className="social-media-icon fa fa-youtube-square fa-5x"></a>
+    <a href="https://twitter.com/LouisLepper" className="social-media-icon fa fa-twitter-square fa-5x"></a>
+    <a href="https://bitbucket.org/louislepper" className="social-media-icon fa fa-bitbucket-square fa-5x"></a>
+    <a href="https://github.com/louislepper" className="social-media-icon fa fa-github-square fa-5x"></a>
+    </div>;
   }
 });
 
 var Resume = React.createClass({
   render: function() {
-    return <a href="resume.pdf">Resume</a>
+    return <div className="resume-link"><a href="resume.pdf">Resume</a></div>
   }
 })
 
 var MainFrame = React.createClass({
   render: function() {
-    return <div><HeaderSection/><ExternalLinksSection/><Resume/></div>;
+    return <div>
+    <Row><HeaderSection/></Row>
+    <Row><ExternalLinksSection/></Row>
+    <Resume/>
+    </div>;
   }
 });
 
@@ -42,6 +60,6 @@ var start = new Date().getTime();
 setInterval(function() {
   ReactDOM.render(
     <MainFrame elapsed={new Date().getTime() - start} />,
-    document.getElementById('container')
+    document.getElementById('container-fluid')
   );
 }, 50);
